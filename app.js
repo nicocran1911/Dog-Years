5372ce5612fb382c9eaabc30aef9beb3a80ba3e7
nicocran1911 <93523661+nicocran1911@users.noreply.github.com>
2021-12-16 14:03:10 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// my age
+const myAge = 27;
+// my early years
+let earlyYears = 2;
+earlyYears *= 10.5;
+// later years
+let laterYears = myAge - 2;
+// later years by 4
+laterYears *= 4;
+console.log(laterYears);
+console.log(earlyYears);
+'Nick'.toLowerCase();
+let myName = 'nicholas'
+console.log(`My name is ${myName}. I am ${earlyYears} years old in dog years.`);
